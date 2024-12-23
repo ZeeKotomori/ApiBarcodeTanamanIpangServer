41ed6c20cccf8e978487d1f2cf5948efce882f1f
ZeeKotomori <zikriaf23@gmail.com>
2024-12-23 15:05:36 +0700

    feat : funct qrTanaman

---
+++ server/src/controllers/qrController.js
@@ -0,0 +1,18 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+exports.qrTanaman = async (req, res) => { 
+    const namaLatin = req.params.qrTanaman;
+
+    try {
+        const tanaman = await prisma.tanaman.findFirst({ where: { namaLatin : namaLatin } });
+        if (tanaman) {
+            res.status(200).json(tanaman);
+        } else {
+            res.status(404).send('Tanaman tidak ditemukan');
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Terjadi kesalahan pada server');
+    }
+};
