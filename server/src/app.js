faee79e83f419bdfede85197bd873739df0ac3ec
ZeeKotomori <zikriaf23@gmail.com>
2024-12-23 15:04:32 +0700

    move : function scan to qrController

--- server/src/app.js
+++ server/src/app.js
@@ -4,27 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const tanamanRoutes = require('./routes/tanamanRoute.js');
+const qrRoutes = require('./routes/qrRoute.js');
 
 const app = express();
 
 app.use(bodyParser.json());
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static('public'));
 
 app.use('/api/tanaman', tanamanRoutes);
-
-app.get('/scan/:qrCode', (req, res) => {
-    const qrCode = req.params.qrCode;
-    const tanamanDetail = tanaman.find(t => t.nama === qrCode);
-    if (tanamanDetail) {
-        res.render('detail', { tanaman: tanamanDetail });
-    } else {
-        res.status(404).send('Tanaman tidak ditemukan');
-    }
-});
+app.use('/scan', qrRoutes);
 
 app.listen(3001, () => {
     console.log('Server berjalan di http://localhost:3001');
