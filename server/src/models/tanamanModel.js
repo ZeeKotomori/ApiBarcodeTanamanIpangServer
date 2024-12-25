986ac949606dd112d0833dd60699ce88c6e9407b
ZeeKotomori <zikriaf23@gmail.com>
2024-12-26 06:20:55 +0700

    delete

--- server/src/models/tanamanModel.js
+++
@@ -1,9 +0,0 @@
-// src/models/tanamanModel.js
-
-const tanaman = [
-    { id: 1, nama: 'mawar', namaLatin: 'Rosa', khasiat: 'Antiseptik', asal: "cianjur" },
-    { id: 2, nama: 'kemangi', namaLatin: 'Ocimum basilicum', khasiat: 'Mengurangi stres' },
-    { id: 3, nama: 'lidah-buaya', namaLatin: 'Aloe vera', khasiat: 'Melembapkan kulit' },
-];
-
-module.exports = tanaman;
