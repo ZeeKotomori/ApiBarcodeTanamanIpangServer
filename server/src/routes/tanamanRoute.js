e3f5603529705319063951702431088c174a8a4a
ZeeKotomori <zikriaf23@gmail.com>
2024-12-28 18:29:17 +0700

    feat new upload.single('file')

--- server/src/routes/tanamanRoute.js
+++ server/src/routes/tanamanRoute.js
@@ -1,23 +1,13 @@
-// src/routes/tanamanRoutes.js
-
-const upload = require('../middleware/uploadMulter.js');
 const express = require('express');
+const upload = require('../middleware/uploadMulter.js');
 const tanamanController = require('../controllers/tanamanController.js');
+
 const router = express.Router();
 
-// Mendapatkan semua tanaman
 router.get('/', tanamanController.getAllTanaman);
-
-// Mendapatkan tanaman berdasarkan id
 router.get('/:id', tanamanController.getTanamanById);
-
-// Menambahkan tanaman baru
 router.post('/', upload.single('file'), tanamanController.createTanaman);
-
-// Mengupdate tanaman berdasarkan id
-router.put('/:id', tanamanController.updateTanaman);
-
-// Menghapus tanaman berdasarkan id
+router.put('/:id', upload.single('file'), tanamanController.updateTanaman);
 router.delete('/:id', tanamanController.deleteTanaman);
 
 module.exports = router;
