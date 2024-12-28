0e62ec9c2842773b72c1eca3fce2ee120d7b2b0c
ZeeKotomori <zikriaf23@gmail.com>
2024-12-28 18:25:29 +0700

    fix (error cors)

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const tanamanRoutes = require('./routes/tanamanRoute.js');
-const cors = require('cors');
 const qrRoutes = require('./routes/qrRoute.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send({ message: 'Terjadi kesalahan pada server' });
-});
 
 app.use(cors({
     origin: '*',
@@ -24,6 +19,11 @@ app.use('/public', express.static('public'));
 
 app.use('/api/tanaman', tanamanRoutes);
 app.use('/scan', qrRoutes);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ message: 'Terjadi kesalahan pada server' });
+});
 
 app.listen(PORT, () => {
     console.log(`Server berjalan di port ${PORT}`);
