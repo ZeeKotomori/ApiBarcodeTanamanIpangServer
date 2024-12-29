12018d1ebad32f5e58d627f23bb0e70e2ba2294b
Virgi <virgiriyadi2404@gmail.com>
2024-12-29 15:21:56 +0700

    Add update feat and new design
    - New Design with Boostrap
    - Add update feat (WIP for file attachment)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,36 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Modal from './components/modals/Modal';
-import AddData from './components/modals/AddData';
+import ModalCenter from './components/modals/Modal'
+import AddDataModal from './components/modals/AddData';
+import EditDataModal from './components/modals/EditData';
+
+// Bootstrap
+import './styles/scss/custom.scss';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
 
 function App() {
    const [tanaman, setTanaman] = useState([]);
-   const [isActive, setIsActive] = useState(false);
+   const [Id, setId] = useState('');
+   const [showModal, setShowModal] = useState(false);
+   const [modalType, setModalType] = useState("");
    const [query, setQuery] = useState('');
 
-   const openModal = () => setIsActive(true);
-   const closeModal = () => setIsActive(false);
+   const handleShowModal = (type, id) => {
+      id && setId(id);
+      setModalType(type);
+      setShowModal(true);
+   }
+
+   const handleCloseModal = () => {
+      setModalType('');
+      setShowModal(false);
+   }
 
    const fetchAPI = (query) => {
       const url = query ? `http://localhost:3001/api/tanaman/${query}` : "http://localhost:3001/api/tanaman";
@@ -44,57 +65,89 @@ function App() {
    }, [query]);
 
    return (
-      <div className="container mt-4">
-         <nav className="panel is-primary">
-            <p className="panel-heading">Tanaman Ipang</p>
-            <div className="panel-block">
-               <p className="control">
-                  <input
-                     className="input"
-                     name='search'
-                     value={query}
-                     type="number"
-                     onChange={(e) => setQuery(e.target.value)}
-                     placeholder="Cari berdasarkan Id" />
-               </p>
-            </div>
-            <div className='panel-block'>
-               <button className='button is-primary' onClick={openModal}>Tambah Data</button>
-            </div>
-            <table className="table is-fullwidth is-hoverable">
+      <Container fluid="lg" className='pt-4'>
+         <Navbar className="bg-white rounded">
+            <Container>
+               <Navbar.Brand href="#">Tanaman Ipang</Navbar.Brand>
+               <Navbar.Collapse className="justify-content-end">
+                  <Navbar.Text>
+                     <a
+                        href="#logout"
+                        className="link-offset-2 link-offset-3-hover link-underline-dark link-underline-opacity-0 link-underline-opacity-75-hover"
+                     >
+                        Log Out
+                     </a>
+                  </Navbar.Text>
+               </Navbar.Collapse>
+            </Container>
+         </Navbar>
+         <Row className='mt-4'>
+            <Form as={Col} xs={4}>
+               <Form.Control
+                  placeholder="Cari berdasarkan Id"
+                  name='search'
+                  value={query}
+                  type="number"
+                  onChange={(e) => setQuery(e.target.value)}
+               />
+            </Form>
+            <Col>
+               <Button type="submit" className='bg-primary'>
+                  Cari
+               </Button>
+            </Col>
+            <Col xs='auto'>
+               <Button variant='primary' onClick={() => handleShowModal('Tambah Data')}>
+                  Tambah Data
+               </Button>
+            </Col>
+         </Row>
+         <Container fluid="lg" className='rounded bg-white p-3 mt-4'>
+            <Table hover responsive="sm" className='rounded'>
                <thead>
                   <tr>
-                     <th>Id</th>
-                     <th>Nama Tanaman</th>
-                     <th>Nama Latin</th>
-                     <th>Khasiat</th>
-                     <th colSpan={2} className='has-text-centered'>Action</th>
+                     <th className='text-center'>Id</th>
+                     <th className='text-center'>Nama Tanaman</th>
+                     <th className='text-center'>Nama Latin</th>
+                     <th className='text-center'>Khasiat</th>
+                     <th className='text-center'>Bagian digunakan</th>
+                     <th colSpan={2} className='text-center'>Aksi</th>
                   </tr>
                </thead>
                <tbody>
                   {tanaman.length > 0 ? (
                      tanaman.map((t, index) => (
                         <tr key={index}>
-                           <td className='is-vcentered'>{t.id}</td>
-                           <td className='is-vcentered'>{t.nama}</td>
-                           <td className='is-vcentered'>{t.namaLatin}</td>
-                           <td className='is-vcentered'>{t.khasiat}</td>
-                           <td><button className="button is-info is-pulled-right">Edit</button></td>
-                           <td><button className="button is-danger" onClick={() => handleDelete(t.id)}>Delete</button></td>
+                           <td className='text-center align-middle'>{t.id}</td>
+                           <td className='text-center align-middle'>{t.nama}</td>
+                           <td className='text-center align-middle'>{t.namaLatin}</td>
+                           <td className='text-center align-middle'>{t.khasiat}</td>
+                           <td className='text-center align-middle'>{t.bagianYangDigunakan}</td>
+                           <td className='text-end'>
+                              <Button variant='light' onClick={() => handleShowModal('edit', t.id)}>Edit</Button>
+                           </td>
+                           <td>
+                              <Button variant='light' onClick={() => handleDelete(t.id)}>Delete</Button>
+                           </td>
                         </tr>
                      ))
                   ) : (
                      <tr>
-                        <td colSpan={5}>Data tidak ditemukan</td>
+                        <td colSpan={6}>Data tidak ditemukan</td>
                      </tr>
                   )}
                </tbody>
-            </table>
-         </nav>
-         <Modal isActive={isActive} closeModal={closeModal}>
-            <AddData />
-         </Modal>
-      </div>
+            </Table>
+         </Container>
+
+         <ModalCenter show={showModal} feature={modalType} onHide={() => handleCloseModal()}>
+            {modalType === "Tambah Data" ? (
+               <AddDataModal></AddDataModal>
+            ) : (
+               <EditDataModal id={Id}></EditDataModal>
+            )}
+         </ModalCenter>
+      </Container>
    )
 }
 
