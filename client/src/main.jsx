51f3324174a84663a40e6ee310aaac5bcb96b04b
Virgi <virgiriyadi2404@gmail.com>
2025-01-04 14:14:07 +0700

    Import Route from react-route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App.jsx'
+import QrCard from './components/cards/QrCard.jsx';
 
 createRoot(document.getElementById('root')).render(
-   <StrictMode>
-      <App />
-   </StrictMode>
+   <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<App />} />
+         <Route path='/print/:file' element={<QrCard />} />
+      </Routes>
+   </BrowserRouter>
 )
